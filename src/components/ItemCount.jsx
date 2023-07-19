609c6fc2c5c7d9b22eeb6acaf54a7adf8b010e0c
OkamiSuarez <okami97.backdev@hotmail.com>
2023-07-19 13:59:57 -0600

    Stock counter added succesfully, waiting for feedback

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -10,9 +10,41 @@ import {
     Divider,
     Center,
 } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 const ItemCount = () => {
     // AQUI IRA LA LOGICA DE PROGRA
+    // let cartCount = 0;
+    const [counter, setCounter] = useState(0)
+    const [stock, setStock] = useState(5)
+    const AddCount=()=>{
+        if(stock <= 0){
+            console.log('stock empty')
+            setCounter(counter)
+        }else{
+            setCounter(counter + 1)
+            setStock(stock - 1)
+        }
+    }
+    const DeleteCount=()=>{
+        if(counter <= 0){
+            setCounter(counter)
+            console.log('No more products to delete from cart')
+        }else{
+            setCounter(counter-1)
+            setStock(stock + 1)
+        }
+    }
+    const ResetCount=()=>{
+        setCounter(0)
+        setStock(5)
+    }
+
+
+
+    useEffect(()=>{
+        console.log('counter active')
+    },[counter])
 
     return (
         <div>
@@ -27,28 +59,31 @@ const ItemCount = () => {
                             <CardBody>
                                 <Heading size="md">
                                     <Center>
-                                    Cart
+                                    Stock of this product is
+                                    </Center>
+                                    <Center>
+                                    <p>{stock}</p>
                                     </Center>
                                 </Heading>
-                                <Center py="2">Your actual stock count is:</Center>
+                                <Center py="2">Your actual cart count is:</Center>
                             </CardBody>
                             
                             <Box position="relative" padding="10">
                                 <Divider />
                                 <AbsoluteCenter bg="white" px="4">
-                                    AQUI VA EL CONTADOR
+                                    <p>{counter}</p>
                                 </AbsoluteCenter>
                             </Box>
 
                             <CardFooter>
                                 <Stack spacing={4} direction='row' align='center'>
-                                <Button variant="solid" colorScheme="green">
+                                <Button variant="solid" colorScheme="green" onClick={AddCount}>
                                     Add
                                 </Button>
-                                <Button variant="solid" colorScheme="red">
+                                <Button variant="solid" colorScheme="red" onClick={DeleteCount}>
                                     Delete
                                 </Button>
-                                <Button variant="solid" colorScheme="black">
+                                <Button variant="solid" colorScheme="black" onClick={ResetCount}>
                                     Reset
                                 </Button>
                                 </Stack>
